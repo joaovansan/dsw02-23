0a843534dba6207551a7b189af1a88e016d4d2a6
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-11-22 13:08:26 +0000

    Feat delete

--- app/admin/course/page.tsx
+++ app/admin/course/page.tsx
@@ -1,8 +1,15 @@
 import { sql } from "@vercel/postgres";
+import { revalidatePath } from "next/cache";
 
 export const revalidate =0
 
 export default async function ListCourse() {
+    async function deleteCourse(formData: FormData){
+        "use server"
+        const id = formData.get("id") as string;
+        await sql`DELETE from courses where id=${id}`
+        revalidatePath("/admin/course")
+    }
     const { rows } = await sql`SELECT * from courses`;
     return (
         <div>
@@ -16,7 +23,15 @@ export default async function ListCourse() {
                     {
                         rows.map((course) => {
                             return (
-                                <tr key={course.id}><td>{course.title}</td> <td>{course.description}</td> </tr>
+                                <tr key={course.id}><td>{course.title}</td> <td>{course.description}</td> 
+                                <td>
+                                    <form >
+                                     <input type="text" hidden name="id" value={course.id}/>   
+                                    <button formAction={deleteCourse}>Excluir</button>
+                                    </form>
+                                
+                                </td> 
+                                </tr>
                             )
                         })
                     }
