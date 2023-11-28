a44231e721fd1611aa121546b05a3adf690a3765
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-11-28 23:42:48 +0000

    Feat inclusao layout

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,7 @@
 export default function Admin(){
     return (
         <div>
-        <h1 className="text-center text-xl">Área Administrativa</h1>
+        <h1 className="text-center text-xl text-color{}">Área Administrativa</h1>
         
         <a href="/admin/course">Listar Curso</a>
         <a href="/admin/course/new">Cadastrar Curso</a>
