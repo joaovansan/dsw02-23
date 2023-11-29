3fd11973dc1fc2edbac8d54737d68e5ed5d1bd8e
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-11-29 00:36:36 +0000

    Fix commit

--- app/admin/course/new/page.tsx
+++ app/admin/course/new/page.tsx
@@ -2,36 +2,35 @@ import UploadButton from "@/app/components/UploadButton";
 import { sql } from "@vercel/postgres";
 import { useSearchParams } from "next/navigation";
 
-export const revalidate =0
+export const revalidate = 0
 
 export default function NewCourse({
-    searchParams,
-  }: {
-    searchParams?: {
-      url?: string;  
-    };
-  }){
-    
-    const urlImage = searchParams?.url || '';
+  searchParams,
+}: {
+  searchParams?: {
+    url?: string;
+  };
+}) {
 
-    async function saveCourse(formData: FormData){
-        "use server"
-        const title = formData.get("title") as string;
-        const description = formData.get("description") as string;
-        await sql`INSERT INTO courses (title, description,url) VALUES(${title}, ${description}, ${urlImage})`
-        console.log("Acessou a função")
-    }
-    return (
-        <div>
-            <h1 className="text-white text-center text-4xl">Cadastrar Cursos</h1>
-            <form>
-                <input type="text" name="title" placeholder="Digite o Título do Curso"/><br/><br/>
-                <input type="text" name="description" placeholder="Digite a Descriçao do curso"/> <br/><br/>
-                <br/>
-                <UploadButton />
-                <button formAction={saveCourse} className="text-white">Salvar</button>
-            </form>
-        </div>
+  const urlImage = searchParams?.url || '';
 
-    )
+  async function saveCourse(formData: FormData) {
+    "use server"
+    const title = formData.get("title") as string;
+    const description = formData.get("description") as string;
+    await sql`INSERT INTO courses (title, description,url) VALUES(${title}, ${description}, ${urlImage})`
+    console.log("Acessou a função")
+  }
+  return (
+    <div>
+      <form>
+        <input type="text" name="title" placeholder="Digite o Título do Curso" /><br /><br />
+        <input type="text" name="description" placeholder="Digite a Descriçao do curso" /> <br /><br />
+        <br />
+        <UploadButton /> <br />
+        <button formAction={saveCourse} className="text-white">Salvar</button>
+      </form>
+    </div>
+
+  )
 }
