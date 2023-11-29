e43db037aedcfd081b839830a30268a97b1fc034
Joao Pedro Vansan <106285651+joaovansan@users.noreply.github.com>
2023-11-29 00:42:32 +0000

    FIX COMMIT

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,21 @@
-import Banner  from "./components/Banner";
+import Banner from "./components/Banner";
 import Courses from "./components/Courses";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 
-export const revalidate=0
+export const revalidate = 0
 export default function Home() {
   return (
-    <>
-      <Header />
-      <Banner />
-      <Courses />
-      <Footer />
+    <div className="bg-[#3d3d3d]">
+      <div className="max-w-[-1440px] m-auto ">
+        <Header />
+        <Banner />
+        <Courses />
+        <Footer />
 
-    </>
+        </div>
+      </div>
 
+    
   )
 }
